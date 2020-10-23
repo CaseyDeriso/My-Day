820cd5581fe470a87759d25536fa08dac3905e9a
Casey <caseyderiso@gmail.com>
2020-10-23 10:44:19 -0500

    created window event that updates styling for table items according to current time

---
+++ Assets/JS/script.js
@@ -0,0 +1,27 @@
+test = false;
+
+let currentHour = 4;
+// moment()
+//   .format("LT") // 7:44 PM
+//   .substring(1, 0); // "7"
+
+$(window).on("mousemove", function () {
+  let timeSlotEl = $("tbody").children();
+
+  for (let i = 0; i < timeSlotEl.length; i++) {
+    // create a table with all possible table color classes
+    let classes = ["table-active", "table-primary", "table-success"];
+    // get reference to current time slot in for loop
+    let currentEl = timeSlotEl[i];
+    // get the ID of the current time slot
+    let currentId = $(currentEl).attr("id");
+    // remove table color classes
+    $(currentEl).removeClass(classes);
+    // change classes if neccesary.
+    if (currentId == currentHour) {
+      $(currentEl).prevAll().addClass("table-active");
+      $(currentEl).addClass("table-primary");
+      $(currentEl).nextAll().addClass("table-light");
+    }
+  }
+});
